1a04542c36b41e36f1606dfd07a1af6ab4b24a7b
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2020-02-24 15:08:50 +0200

    Creating visitor class

---
+++ core/src/fi/tuni/tamk/tiko/kingsfeast/Guest.java
@@ -0,0 +1,28 @@
+package fi.tuni.tamk.tiko.kingsfeast;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Guest {
+    private String name;
+    private String title;
+    private Sprite visitorSprite;
+    private String[] likes;
+    private String[] dislikes;
+    private String[] dialogue;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
