6135035e5d4027c2c10a2964075dea6715490f4b
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-02-24 23:16:32 +0200

    Add possibility to test BodyBuilder class; DON'T MERGE THIS TO MASTER!!
    Seems to work fine. REVERT TO PREVIOUS BEFORE MERGING!!

--- core/src/fi/tuni/tamk/tiko/kingsfeast/Main.java
+++ core/src/fi/tuni/tamk/tiko/kingsfeast/Main.java
@@ -1,73 +1,108 @@
 package fi.tuni.tamk.tiko.kingsfeast;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Main extends ApplicationAdapter {
-	// TODO: Add documentation everywhere.
+    // TODO: Add documentation everywhere.
 
-	private final boolean DEBUG_PHYSICS = true;
+    private final boolean DEBUG_PHYSICS = true;
 
-	// Initial values, work in progress. Pixels -> meters.
-	private final float unitScale = 1 / 100f;
-	private final float GAME_WIDTH = 800 * unitScale;
-	private final float GAME_HEIGHT = 450 * unitScale;
-	private final Vector2 gravity = new Vector2(0, -9.8f);
+    // Initial values, work in progress. Pixels -> meters.
+    private final float unitScale = 1 / 100f;
+    private final float GAME_WIDTH = 800 * unitScale;
+    private final float GAME_HEIGHT = 450 * unitScale;
+    private final Vector2 gravity = new Vector2(0, -9.8f);
 
-	private SpriteBatch batch;
-	private Texture img;
-	private TiledMap tiledMap;
-	private TiledMapRenderer tiledMapRenderer;
-	private OrthographicCamera camera;
-	private World world;
-	private Box2DDebugRenderer box2DDebugRenderer;
-	private WorldContactListener worldContactListener;
+    private SpriteBatch batch;
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+    private OrthographicCamera camera;
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+    private WorldContactListener worldContactListener;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		world = new World(gravity, false);
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        world = new World(gravity, false);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
 
-		// tiledMap
-		// tiledMapRenderer
+        tiledMap = new TmxMapLoader().load("transformtestmap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, unitScale);
 
-		box2DDebugRenderer = new Box2DDebugRenderer();
+        BodyBuilder.transformObjectsToBodies(tiledMap,
+                world,
+                "ground-collision",
+                "floor",
+                unitScale);
 
-		worldContactListener = new WorldContactListener();
-		world.setContactListener(worldContactListener);
+        BodyBuilder.transformObjectsToBodies(tiledMap,
+                world,
+                "wall-collision",
+                "wall",
+                unitScale);
 
-		img = new Texture("badlogic.jpg");
-	}
+        BodyBuilder.transformObjectsToBodies(tiledMap,
+                world,
+                "death-layer",
+                "spikes",
+                unitScale);
 
-	@Override
-	public void render () {
-		batch.setProjectionMatrix(camera.combined);
-		Util.clearScreen();
+        box2DDebugRenderer = new Box2DDebugRenderer();
 
-		batch.begin();
-		batch.draw(img, 0, 0,
-				img.getWidth() * unitScale,
-				img.getHeight() * unitScale);
+        worldContactListener = new WorldContactListener();
+        world.setContactListener(worldContactListener);
+    }
 
-		batch.end();
+    @Override
+    public void render() {
+        batch.setProjectionMatrix(camera.combined);
+        Util.clearScreen();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
 
-		if(DEBUG_PHYSICS) {
-			box2DDebugRenderer.render(world, camera.combined);
-		}
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        moveCamera();
+        camera.update();
+
+        batch.begin();
+        batch.end();
+
+        if (DEBUG_PHYSICS) {
+            box2DDebugRenderer.render(world, camera.combined);
+        }
+
+        world.step(1 / 60f, 8, 3);
+    }
+
+    public void moveCamera() {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            camera.translate(0.0f, 0.4f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            camera.translate(0.0f, -0.4f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            camera.translate(0.4f, 0.0f);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            camera.translate(-0.4f, 0.0f);
+        }
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
 }
