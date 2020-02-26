9c3c4c368193d498374304643e87433559a11480
Aleksi Muikku <aleksi.muikku@hotmail.com>
2020-02-26 11:08:52 +0200

    Add world stepping / Correct indentation tabs -> 4 spaces

--- core/src/fi/tuni/tamk/tiko/kingsfeast/Main.java
+++ core/src/fi/tuni/tamk/tiko/kingsfeast/Main.java
@@ -1,6 +1,7 @@
 package fi.tuni.tamk.tiko.kingsfeast;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,63 +12,65 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Main extends ApplicationAdapter {
-	// TODO: Add documentation everywhere.
+    // TODO: Add documentation everywhere.
 
-	private final boolean DEBUG_PHYSICS = true;
+    private final boolean DEBUG_PHYSICS = true;
 
-	// Initial values, work in progress. Pixels -> meters.
-	private final float unitScale = 1 / 100f;
-	private final float GAME_WIDTH = 800 * unitScale;
-	private final float GAME_HEIGHT = 450 * unitScale;
-	private final Vector2 gravity = new Vector2(0, -9.8f);
+    // Initial values, work in progress. Pixels -> meters.
+    private final float unitScale = 1 / 100f;
+    private final float GAME_WIDTH = 800 * unitScale;
+    private final float GAME_HEIGHT = 450 * unitScale;
+    private final Vector2 gravity = new Vector2(0, -9.8f);
 
-	private SpriteBatch batch;
-	private Texture img;
-	private TiledMap tiledMap;
-	private TiledMapRenderer tiledMapRenderer;
-	private OrthographicCamera camera;
-	private World world;
-	private Box2DDebugRenderer box2DDebugRenderer;
-	private WorldContactListener worldContactListener;
+    private SpriteBatch batch;
+    private Texture img;
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+    private OrthographicCamera camera;
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+    private WorldContactListener worldContactListener;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		world = new World(gravity, false);
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        world = new World(gravity, false);
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, GAME_WIDTH, GAME_HEIGHT);
 
-		// tiledMap
-		// tiledMapRenderer
+        // tiledMap
+        // tiledMapRenderer
 
-		box2DDebugRenderer = new Box2DDebugRenderer();
+        box2DDebugRenderer = new Box2DDebugRenderer();
 
-		worldContactListener = new WorldContactListener();
-		world.setContactListener(worldContactListener);
+        worldContactListener = new WorldContactListener();
+        world.setContactListener(worldContactListener);
 
-		img = new Texture("badlogic.jpg");
-	}
+        img = new Texture("badlogic.jpg");
+    }
 
-	@Override
-	public void render () {
-		batch.setProjectionMatrix(camera.combined);
-		Util.clearScreen();
+    @Override
+    public void render () {
+        batch.setProjectionMatrix(camera.combined);
+        Util.clearScreen();
 
-		batch.begin();
-		batch.draw(img, 0, 0,
-				img.getWidth() * unitScale,
-				img.getHeight() * unitScale);
+        batch.begin();
+        batch.draw(img, 0, 0,
+                img.getWidth() * unitScale,
+                img.getHeight() * unitScale);
 
-		batch.end();
+        batch.end();
 
-		if(DEBUG_PHYSICS) {
-			box2DDebugRenderer.render(world, camera.combined);
-		}
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        if(DEBUG_PHYSICS) {
+            box2DDebugRenderer.render(world, camera.combined);
+        }
+
+        Util.worldStep(world, Gdx.graphics.getDeltaTime());
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+    }
 }
--- core/src/fi/tuni/tamk/tiko/kingsfeast/Util.java
+++ core/src/fi/tuni/tamk/tiko/kingsfeast/Util.java
@@ -2,12 +2,31 @@ package fi.tuni.tamk.tiko.kingsfeast;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Supposed to contain universally useful methods. Meant to reduce cluttering
  * of Main class.
  */
 public class Util {
+
+    private static double accumulator = 0;
+    private static float TIME_STEP = 1 / 60f;
+    // Jussi's method for world stepping
+    public static void worldStep(World world, float deltaTime) {
+        float frameTime = deltaTime;
+
+        if(deltaTime > 1 / 4f) {
+            frameTime = 1 / 4f;
+        }
+
+        accumulator += frameTime;
+
+        while(accumulator >= TIME_STEP) {
+            world.step(TIME_STEP, 8, 3);
+            accumulator -= TIME_STEP;
+        }
+    }
 
     public static void clearScreen() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
